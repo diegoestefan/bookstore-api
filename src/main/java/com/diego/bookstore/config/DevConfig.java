b9f9c991e09922c20b1e9b64572337062b9b8091
diegoestefan <diego.estefanc@gmail.com>
2023-06-04 18:23:28 -0300

    novo user de teste

---
+++ src/main/java/com/diego/bookstore/config/DevConfig.java
@@ -0,0 +1,28 @@
+package com.diego.bookstore.config;
+
+import com.diego.bookstore.service.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean initBD(){
+        if(strategy.equals("create")){
+            this.dbService.initBD();
+        }
+        return false;
+    }
+
+}
